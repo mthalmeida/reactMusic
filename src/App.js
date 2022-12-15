7bca3968601b19e996666a06e07ab95d5dbce8db
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-12-15 18:27:57 -0300

    feat: adicionando arquivos em novo repositorio

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter basename={ process.env.PUBLIC_URL }>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
